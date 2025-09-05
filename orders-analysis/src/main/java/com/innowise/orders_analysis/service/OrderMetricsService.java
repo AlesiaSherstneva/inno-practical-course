3af6717004f8bfde38e0fd86845c68d7e33c7202
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-09-05 07:12:37 +0300

    Write class with methods according to the task

---
+++ orders-analysis/src/main/java/com/innowise/orders_analysis/service/OrderMetricsService.java
@@ -0,0 +1,70 @@
+package com.innowise.orders_analysis.service;
+
+import com.innowise.orders_analysis.model.Customer;
+import com.innowise.orders_analysis.model.Order;
+import com.innowise.orders_analysis.model.OrderItem;
+import com.innowise.orders_analysis.model.enums.OrderStatus;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OrderMetricsService {
+    public List<String> getUniqueOrderCities(List<Order> orders) {
+        return orders.stream()
+                .map(Order::getCustomer)
+                .map(Customer::getCity)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public double countTotalCompletedIncome(List<Order> orders) {
+        return orders.stream()
+                .filter(order -> order.getStatus().equals(OrderStatus.DELIVERED))
+                .mapToDouble(order -> calculateTotalAmount(order.getItems()))
+                .sum();
+    }
+
+    public String findBestSellingProduct(List<Order> orders) {
+        return orders.stream()
+                .filter(order -> order.getStatus() != OrderStatus.CANCELLED)
+                .flatMap(order -> order.getItems().stream())
+                .collect(Collectors.groupingBy(
+                        OrderItem::getProductName, Collectors.summingInt(OrderItem::getQuantity))
+                )
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey).orElse("No products found");
+    }
+
+    public double calculateDeliveredOrdersAverageCheck(List<Order> orders) {
+        List<Order> deliveredOrders = orders.stream()
+                .filter(order -> order.getStatus().equals(OrderStatus.DELIVERED))
+                .toList();
+
+        if (deliveredOrders.isEmpty()) {
+            return 0.00;
+        }
+
+        double totalAmount = deliveredOrders.stream()
+                .mapToDouble(order -> calculateTotalAmount(order.getItems()))
+                .reduce(0, Double::sum);
+
+        return totalAmount / deliveredOrders.size();
+    }
+
+    public List<Customer> findCustomersWithFiveOrMoreOrders(List<Order> orders) {
+        return orders.stream()
+                .collect(Collectors.groupingBy(Order::getCustomer, Collectors.counting()))
+                .entrySet().stream()
+                .filter(es -> es.getValue() >= 5)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    private double calculateTotalAmount(List<OrderItem> orderItems) {
+        return orderItems.stream()
+                .mapToDouble(item -> item.getPrice() * item.getQuantity())
+                .sum();
+    }
+}
