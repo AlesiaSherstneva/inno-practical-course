246b918d48d2452e29d2282562a5a2e3dd1c92f4
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-09-10 10:20:19 +0300

    Write unit tests

---
+++ skynet/src/test/java/com/innowise/skynet/runner/SimulationRunnerTest.java
@@ -0,0 +1,39 @@
+package com.innowise.skynet.runner;
+
+import com.innowise.skynet.faction.Wednesday;
+import com.innowise.skynet.faction.World;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimulationRunnerTest {
+    private SimulationRunner runner;
+
+    @BeforeEach
+    void setUp() {
+        runner = new SimulationRunner();
+    }
+
+    @Test
+    void runSimulationTest() throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            runner.runSimulation();
+
+            int worldArmy = runner.getWorldArmySize();
+            int wednesdayArmy = runner.getWednesdayArmySize();
+
+            String simulationResult = runner.getWinner();
+
+            String expectedSimulationResult;
+            if (worldArmy == wednesdayArmy) {
+                expectedSimulationResult = "Draw, no winner";
+            } else {
+                expectedSimulationResult = String.format("The winner is %s faction", worldArmy > wednesdayArmy
+                        ? World.class.getSimpleName() : Wednesday.class.getSimpleName());
+            }
+
+            assertEquals(expectedSimulationResult, simulationResult);
+        }
+    }
+}
