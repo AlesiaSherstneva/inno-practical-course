247739fd4d216c0a832072c6568d4abba260619e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-09-16 16:18:04 +0300

    Code review fixes: expand unit tests

--- skynet/src/test/java/com/innowise/skynet/runner/SimulationRunnerTest.java
+++ skynet/src/test/java/com/innowise/skynet/runner/SimulationRunnerTest.java
@@ -1,10 +1,13 @@
 package com.innowise.skynet.runner;
 
 import com.innowise.skynet.SimulationRunner;
-import com.innowise.skynet.service.faction.Wednesday;
-import com.innowise.skynet.service.faction.World;
+import com.innowise.skynet.dto.SimulationResultDto;
+import com.innowise.skynet.service.faction.impl.Wednesday;
+import com.innowise.skynet.service.faction.impl.World;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,22 +22,30 @@ class SimulationRunnerTest {
     @Test
     void runSimulationTest() throws InterruptedException {
         for (int i = 0; i < 5; i++) {
-            runner.runSimulation();
-
-            int worldArmy = runner.getWorldArmySize();
-            int wednesdayArmy = runner.getWednesdayArmySize();
-
-            String simulationResult = runner.getWinner();
-
-            String expectedSimulationResult;
-            if (worldArmy == wednesdayArmy) {
-                expectedSimulationResult = "Draw, no winner";
-            } else {
-                expectedSimulationResult = String.format("The winner is %s faction", worldArmy > wednesdayArmy
-                        ? World.class.getSimpleName() : Wednesday.class.getSimpleName());
-            }
-
-            assertEquals(expectedSimulationResult, simulationResult);
+            checkSimulationResults(runner.runSimulation());
         }
+    }
+
+    @Test
+    void runSeveralSimulationsTest() throws InterruptedException {
+        List<SimulationResultDto> simulationResults = runner.runSimulation(5);
+
+        for (SimulationResultDto simulationResult: simulationResults) {
+            checkSimulationResults(simulationResult);
+        }
+    }
+
+    private void checkSimulationResults(SimulationResultDto simulationResult) {
+        String expectedSimulationResult;
+
+        if (simulationResult.getWorldArmySize() == simulationResult.getWednesdayArmySize()) {
+            expectedSimulationResult = "Draw, no winner";
+        } else {
+            expectedSimulationResult = String.format("The winner is %s faction",
+                    simulationResult.getWorldArmySize() > simulationResult.getWednesdayArmySize()
+                    ? World.class.getSimpleName() : Wednesday.class.getSimpleName());
+        }
+
+        assertEquals(expectedSimulationResult, simulationResult.getWinner());
     }
 }
